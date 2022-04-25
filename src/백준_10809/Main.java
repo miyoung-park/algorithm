f2114df96b6b5e1922c408ee819a3cf21775751f
miyoung-park <qkraldud1529@gmail.com>
2022-04-25 17:51:07 +0900

    upload

---
+++ src/백준_10809/Main.java
@@ -0,0 +1,12 @@
+package 백준_10809;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String[] strArr = sc.next().split("");
+        String[] alphabetArr = { "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","q","r","s","t","u","v","w","x","y","z" };
+
+    }
+}
---
+++ src/백준_1157/Main.java
@@ -0,0 +1,7 @@
+package 백준_1157;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
