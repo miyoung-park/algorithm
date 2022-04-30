b9adfda2d7549ba8486d380ff6bb34512ba276de
miyoung-park <qkraldud1529@gmail.com>
2022-04-30 10:56:28 +0900

    upload

---
+++ src/백준_10845/Main.java
@@ -0,0 +1,50 @@
+package 백준_10845;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt( br.readLine() );
+        ArrayList<Integer> stack = new ArrayList<>();
+
+        for( int i = 0; i < N; i++ ){
+            String order = br.readLine();
+
+            if( order.contains("push") ){
+                String[] pushArr = order.split(" ");
+                stack.add( Integer.parseInt(pushArr[pushArr.length-1]) );
+            }
+
+            if( order.contains("pop") ){
+                if( stack.size() == 0 ){
+                    System.out.println(-1);
+                    continue;
+                }
+                System.out.println( stack.get(0));
+                stack.remove(0);
+            }
+
+            if( order.contains("size") ){
+                System.out.println( stack.size() );
+            }
+
+            if( order.contains("empty") ){
+                int result = stack.size() == 0 ? 1 : 0;
+                System.out.println( result );
+            }
+
+            if( order.contains("front") ){
+                int result = stack.size() == 0 ?  -1 : stack.get(0);
+                System.out.println( result );
+            }
+
+            if( order.contains("back") ){
+                int result = stack.size() == 0 ?  -1 : stack.get(stack.size() - 1);
+                System.out.println( result );
+            }
+        }
+    }
+}
