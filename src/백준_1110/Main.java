e8697cc505a0eabfa10e0d8ae6512b806c1f0082
miyoung-park <qkraldud1529@gmail.com>
2022-05-30 16:59:11 +0900

    upload

--- src/백준_1110/Main.java
+++ src/백준_1110/Main.java
@@ -1,21 +1,33 @@
 package 백준_1110;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main( String[] args ){
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+        String num = sc.next();
+        int index = 0;
+        if( num.split("").length == 1 ) {
+            num = "0" + num;
+        }
+        String[] originNum = num.split("");
+        String[] numArr = originNum;
+        int sum;
+        String[] sumArr;
+        String stringSum;
 
         while( true ){
-            // num 이 10 보다 작을 경우 0을 붙여서 계산하게 되는데 결국엔 *11 한 것과 같아짐
-            num = num < 10 ? num * 11 : num;
-            String a = String.valueOf(num).split("")[0];
-            String b = String.valueOf(num).split("")[1];
-        }
+            sum = ( Integer.parseInt(numArr[0]) + Integer.parseInt(numArr[1]) ); //  8
+            sumArr = String.valueOf(sum).split(""); //  { 8 }
+            stringSum = numArr[numArr.length - 1] + sumArr[sumArr.length - 1];
+            numArr = stringSum.split("");
+            index ++;
 
+            if( (originNum[0] + originNum[1]).equals( stringSum ) ){
+                break;
+            }
+        }
+        System.out.println(index);
     }
 }
