4fdfe8a35c4d1727a5739d02dc604bbf26b6e57a
miyoung-park <qkraldud1529@gmail.com>
2022-06-08 18:08:46 +0900

    upload

--- src/백준_1157/Main.java
+++ src/백준_1157/Main.java
@@ -1,7 +1,15 @@
 package 백준_1157;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next().toUpperCase();
+        int[] numberArr = new int[26];
 
+        for( int i = 0; i < str.length(); i++ ){
+            numberArr[(byte)str.charAt(i) - 65] += 1;
+        }
     }
 }
