8f552cdf43b63b6f8badc788544ef03924f01ab6
miyoung-park <qkraldud1529@gmail.com>
2022-06-13 17:23:32 +0900

    upload

---
+++ src/백준_10179/Main.java
@@ -0,0 +1,15 @@
+package 백준_10179;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int cycle = sc.nextInt();
+        for( int i = 0; i<cycle; i++ ){
+            double dollar = sc.nextDouble();
+            String result = String.format("%.2f" , dollar * 0.8);
+            System.out.println( "$" + result );
+        }
+    }
+}
