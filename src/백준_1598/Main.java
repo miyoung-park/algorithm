53a98c9236a78c1f1e76ffdbbce84cd881b64086
miyoung-park <qkraldud1529@gmail.com>
2022-06-14 15:41:49 +0900

    upload

--- src/백준_1598/Main.java
+++ src/백준_1598/Main.java
@@ -1,15 +1,19 @@
 package 백준_1598;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int from = sc.nextInt();
         int to = sc.nextInt();
-        int[] fromArr = new int[2];
-        int[] toArr = new int[2];
-        if( )
+        int[][] numberArr = {{from % 4, from / 4 + 1}, {to % 4, to / 4 + 1}};
+        for (int i = 0; i < numberArr.length; i++) {
+            if ( numberArr[i][0] % 4 == 0)  {
+                numberArr[i][0] = 4;
+                numberArr[i][1] -= 1;
+            }
+        }
+        System.out.println(Math.abs(numberArr[0][1] - numberArr[1][1]) + Math.abs(numberArr[0][0] - numberArr[1][0]));
     }
 }
