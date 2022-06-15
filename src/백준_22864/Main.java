7939f8b77879cb4430a83788f7da3ac9f25a0ffa
miyoung-park <qkraldud1529@gmail.com>
2022-06-15 15:36:39 +0900

    upload

--- src/백준_22864/Main.java
+++ src/백준_22864/Main.java
@@ -5,15 +5,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        // 한 시간 기준(하루 24시간)
         int A = sc.nextInt(); // 피로도 상승
         int B = sc.nextInt(); // 처리가능
         int C = sc.nextInt(); // 피로도 하락
         int M = sc.nextInt(); // 번아웃 경계
-        int MAX = 0; // 일할 수 있는 최대 시간
+        int MaxA = 0; // 피로도
+        int MaxB = 0; // 처리할 수 있는 일
 
-        while( true ){
+        for( int i=0; i<24; i++ ){
+            if( M < (MaxA + A) ) {
+                MaxA = MaxA - C;
+                MaxA = MaxA < 0 ? 0 : MaxA;
+            } else {
+                MaxA += A;
+                MaxB += B;
+            }
         }
-
+        System.out.println(MaxB);
     }
 }
