6852192a0ad8470781d722df56d7ab3316a8aefe
miyoung-park <qkraldud1529@gmail.com>
2022-07-14 17:51:33 +0900

    upload

---
+++ src/백준_1000_10000/백준_6322/Main.java
@@ -0,0 +1,24 @@
+package 백준_1000_10000.백준_6322;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int x = 0;
+        int y = 0;
+        if( a >= c || c >= c ){
+            System.out.println("Impossible.");
+        } else if( a == -1 ) {
+            System.out.println();
+        } else if( b == -1 ) {
+
+        } else if( c == -1 ) {
+
+        }
+
+    }
+}
