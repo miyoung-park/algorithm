4883322e584e230ac9c124610b7fc9d47a86253c
miyoung-park <qkraldud1529@gmail.com>
2022-08-03 11:19:18 +0900

    upload

--- src/백준_1000_10000/백준_5618/Main.java
+++ src/백준_1000_10000/백준_5618/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int c = sc.nextInt();
+
         List<Integer> list = new ArrayList<>();
         List<Integer> factors = new ArrayList<>();
         for( int i=0 ; i<c; i++ ){
@@ -15,10 +16,10 @@ public class Main {
         for( int i=1; i<=min; i++ ) {
             if( min % i == 0 ) factors.add(i);
         }
-        for( int num : list ){
-            for( int j=0; j<factors.size(); j++ ){
-                if( num % factors.get(j) != 0 ){
-                    factors.remove(factors.get(j));
+        for( int i=0; i< list.size(); i++ ){
+            for( int j=factors.size()-1; j>=0; j-- ){
+                if( list.get(i) % factors.get(j) != 0 ){
+                    factors.remove(j);
                 }
             }
         }
