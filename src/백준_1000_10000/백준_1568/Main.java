5c002f3dced2639bc8375ae69375c35ab865426e
miyoung-park <qkraldud1529@gmail.com>
2022-08-22 17:24:55 +0900

    upload

--- src/백준_1000_10000/백준_1568/Main.java
+++ src/백준_1000_10000/백준_1568/Main.java
@@ -1,11 +1,20 @@
 package 백준_1000_10000.백준_1568;
 
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        
+        int birds = sc.nextInt();
+        int time = 0;
+        int count = 0;
+        while( true ){
+            if( birds - time < 0 ) time = 1;
+            birds -= time;
+            if( birds == 0 ) break;
+            count++;
+            time++;
+        }
+        System.out.println(count);
     }
 }
