141b2134a9723e7edc635ec387cca8b1f7623809
miyoung-park <qkraldud1529@gmail.com>
2022-08-22 11:23:47 +0900

    upload

---
+++ src/백준_1000_10000/백준_1568/Main.java
@@ -0,0 +1,11 @@
+package 백준_1000_10000.백준_1568;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
